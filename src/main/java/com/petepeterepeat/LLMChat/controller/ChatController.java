0c1b9261be322204c1558dab5382517a532d22a4
Peter Woodhead <peter2008john@gmail.com>
2023-10-03 16:40:01 +1100

    Prompt-response stream working!

--- src/main/java/com/petepeterepeat/LLMChat/controller/ChatController.java
+++ src/main/java/com/petepeterepeat/LLMChat/controller/ChatController.java
@@ -1,43 +1,15 @@
-//package com.petepeterepeat.LLMChat.controller;
-//
-//import com.petepeterepeat.LLMChat.model.ApiResponse;
-//import com.petepeterepeat.LLMChat.service.OllamaService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//
-//@Controller
-//public class ChatController {
-//    @Autowired
-//    private OllamaService ollamaService;
-//
-//    @GetMapping("/")
-//    public String chat(Model model) {
-//        return "chat";
-//    }
-//
-//    @PostMapping("/send")
-//    public String sendMessage(@RequestParam String prompt, Model model) {
-//        ApiResponse response = ollamaService.sendPrompt("mistral:latest", prompt);
-//        model.addAttribute("response", response.getResponse());
-//        return "chat";
-//    }
-//}
-
-
 package com.petepeterepeat.LLMChat.controller;
 
 import com.petepeterepeat.LLMChat.model.ApiResponse;
 import com.petepeterepeat.LLMChat.service.OllamaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Controller
@@ -52,8 +24,16 @@ public class ChatController {
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String prompt, Model model) {
-        Mono<ApiResponse> response = ollamaService.sendPrompt("mistral:latest", prompt);
-        model.addAttribute("response", response.block().getResponse());  // Note: blocking here, in reality, you'd want more reactive flow
+        Flux<ApiResponse> responses = ollamaService.sendPrompt("mistral:latest", prompt);
+
+        // Store the Flux in the model to be accessed in JavaScript
+        model.addAttribute("responses", responses);
         return "chat";
+    }
+
+    // SSE endpoint to stream responses
+    @GetMapping(value = "/streamResponses", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<ApiResponse> streamResponses(@RequestParam String prompt) {
+        return ollamaService.sendPrompt("mistral:latest", prompt);
     }
 }

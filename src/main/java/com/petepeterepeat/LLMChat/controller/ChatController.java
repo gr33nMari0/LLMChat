e41e47453aded5d5ea4f357e02b5647098a00cba
Peter Woodhead <peter2008john@gmail.com>
2023-10-03 16:08:16 +1100

    Added context-aware prompting - needs fixing

--- src/main/java/com/petepeterepeat/LLMChat/controller/ChatController.java
+++ src/main/java/com/petepeterepeat/LLMChat/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.petepeterepeat.LLMChat.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.petepeterepeat.LLMChat.model.ApiResponse;
 import com.petepeterepeat.LLMChat.service.OllamaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class ChatController {
+
     @Autowired
     private OllamaService ollamaService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping("/")
     public String chat(Model model) {
@@ -23,8 +31,9 @@ public class ChatController {
     }
 
     @PostMapping("/send")
-    public String sendMessage(@RequestParam String prompt, Model model) {
-        Flux<ApiResponse> responses = ollamaService.sendPrompt("mistral:latest", prompt);
+    public String sendMessage(@RequestParam String prompt, Model model, String unParsedContext) {
+        List<Integer> context = parseContext(unParsedContext);
+        Flux<ApiResponse> responses = determineServiceCall(prompt, context);
 
         // Store the Flux in the model to be accessed in JavaScript
         model.addAttribute("responses", responses);
@@ -33,9 +42,30 @@ public class ChatController {
 
     // SSE endpoint to stream responses
     @GetMapping(value = "/streamResponses", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ApiResponse> streamResponses(@RequestParam String prompt) {
-        return ollamaService.sendPrompt("mistral:latest", prompt)
-                .takeUntil(apiResponse -> apiResponse.isDone());
+    public Flux<ApiResponse> streamResponses(@RequestParam String prompt, String unParsedContext) {
+        List<Integer> context = parseContext(unParsedContext);
+        Flux<ApiResponse> responses = determineServiceCall(prompt, context);
+        return responses.takeUntil(apiResponse -> apiResponse.isDone());
     }
 
+    private List<Integer> parseContext(String unParsedContext) {
+        if (unParsedContext != null && !unParsedContext.isEmpty()) {
+            Integer[] contextArray;
+            try {
+                contextArray = objectMapper.readValue(unParsedContext, Integer[].class);
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+            return Arrays.asList(contextArray);
+        }
+        return null;
+    }
+
+    private Flux<ApiResponse> determineServiceCall(String prompt, List<Integer> context) {
+        if (context == null || context.isEmpty()) {
+            return ollamaService.sendPrompt("mistral:latest", prompt);
+        } else {
+            return ollamaService.sendPrompt("mistral:latest", prompt, context);
+        }
+    }
 }
--- src/main/java/com/petepeterepeat/LLMChat/service/OllamaService.java
+++ src/main/java/com/petepeterepeat/LLMChat/service/OllamaService.java
@@ -1,26 +1,57 @@
 package com.petepeterepeat.LLMChat.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.petepeterepeat.LLMChat.model.ApiResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class OllamaService {
     private final String API_URL = "http://localhost:11434/api";
     private final WebClient webClient;
+    private final ObjectMapper objectMapper;
 
     public OllamaService() {
         this.webClient = WebClient.create(API_URL);
+        this.objectMapper = new ObjectMapper();
     }
 
     public Flux<ApiResponse> sendPrompt(String model, String prompt) {
-        return webClient.post()
-                .uri(uriBuilder -> uriBuilder.path("/generate").build())
-                .body(BodyInserters.fromValue(String.format("{\"model\": \"%s\",\"prompt\": \"%s\"}", model, prompt)))
-                .exchange()
-                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(ApiResponse.class));
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("model", model);
+        requestBody.put("prompt", prompt);
+
+        return sendRequest(requestBody);
     }
 
+    public Flux<ApiResponse> sendPrompt(String model, String prompt, List<Integer> context) {
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("model", model);
+        requestBody.put("prompt", prompt);
+        requestBody.put("context", context);
+
+        return sendRequest(requestBody);
+    }
+
+    private Flux<ApiResponse> sendRequest(Object requestBody) {
+        try {
+            String jsonPayload = objectMapper.writeValueAsString(requestBody);
+
+            return webClient.post()
+                    .uri(uriBuilder -> uriBuilder.path("/generate").build())
+                    .body(BodyInserters.fromValue(jsonPayload))
+                    .exchange()
+                    .flatMapMany(clientResponse -> clientResponse.bodyToFlux(ApiResponse.class));
+        } catch (Exception e) {
+            // Handle the exception appropriately
+            e.printStackTrace();
+            return Flux.error(e);
+        }
+    }
 }
